6283b63250c9c6d88889803c2eebc595d8c878b9
VISHALX33 <vishalprajapati2303@gmail.com>
2024-09-02 18:05:56 +0530

    creating an Todo list for users

---
+++ ToDo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import TodoList from "./TodoList";
+import "./App.css"
+
+function App() {
+  
+
+  return (
+    <>
+    <TodoList />
+    </>
+       
+      
+    
+  );
+}
+    
+export default App;
---
+++ ToDo/src/TodoList.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+export default function TodoList() {
+    let [todos, setTodos] = useState(["sample Task"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        setTodos([...todos,newTodo]);
+        setNewTodo("");
+    };
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    }
+    return (
+        <div>
+            <input placeholder="Add a task" value={newTodo} 
+            onChange={updateTodoValue}></input>
+            <br></br>
+            <button onClick={addNewTask}>Add Task</button>
+            <br></br>
+            <br></br>
+            <br></br>
+
+
+            <hr></hr>
+            <h4>ToDo list </h4>
+
+            <ul>
+                {todos.map((todo) => (
+                    <li>{todo}</li>
+                ))}
+            </ul>
+
+        </div>
+    );
+}
